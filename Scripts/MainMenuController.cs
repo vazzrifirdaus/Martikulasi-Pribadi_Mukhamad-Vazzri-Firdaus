05d1845579d22bd7e6f7d56929d1c9f55aa272b7
vazzrifirdaus <142162588+vazzrifirdaus@users.noreply.github.com>
2023-08-23 11:30:24 +0700

    Initial commit

---
+++ Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button playButton;
+    public Button exitButton;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("PinBall_Game");
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
